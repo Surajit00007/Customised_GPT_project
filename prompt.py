622b547ccfc3fa347d46a9da1b07ff0065eb959d
Surajit Sahoo <162908335+Surajit00007@users.noreply.github.com>
2025-07-11 16:13:12 +0530

    Updated prompt.py

--- prompt.py
+++ prompt.py
@@ -1,5 +1,4 @@
-system_prompt = "You are a Smart Chatbot. Answer clearly and concise. Dont say or ask extra follow up question. just write the answer in one word or in one sentence." \
-" I want output message in Hindi language"
+system_prompt = "You are a Smart Chatbot. Answer clearly and concise. Dont say or ask extra follow up question. just write the answer in one word or in one sentence." 
 
 #base prompt
 #few examples related to the prompt
